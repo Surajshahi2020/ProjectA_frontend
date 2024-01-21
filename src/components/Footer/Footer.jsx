883d969b854a914d1976de009e16e8e086726682
Surajshahi2020 <suraj.171245@ncit.edu.np>
2024-01-21 09:48:19 +0545

    footer added successfully

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='bg-color'>
+            <div className='Footer flex'>
+            <p>© Nepal Army Technical Department 2023 | All Rights Reserved</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Content from '../../components/Content/Content';
+import Footer from '../../components/Footer/Footer';
 
 function Home() {
     return (
         <div>
             <Navbar />
             <Content />
+            <Footer />
         </div>
     );
 }
